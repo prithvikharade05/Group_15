4b4c058e6da19f4378d6ff0c699dda5383134fd8
prithvikharade05 <prithvikharade05@gmail.com>
2026-03-25 01:48:53 +0530

    backend updates and integration work

--- core/settings.py
+++ core/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alphamind_db',
         'USER': 'postgres',
-        'PASSWORD': 'bhairavi',
+        'PASSWORD': 'prithvi',
         'HOST': 'localhost',
         'PORT': '5432',
     }
