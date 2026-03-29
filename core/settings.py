dd4aa7cc2ff5b74cc56ca4c782e2b78b43de314e
prithvikharade05 <prithvikharade05@gmail.com>
2026-03-30 02:30:42 +0530

    TwelveData added

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-c6s0@00q38v0wx=vkp9p7vu)^ajg54d99ml_k9mdl)z=rm+o89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["alphamindpro.duckdns.org",
+ALLOWED_HOSTS = ["alphamindpro.duckdns.org","52.141.2.69",
     "127.0.0.1",
     "localhost"]
 
